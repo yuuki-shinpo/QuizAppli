8f6ced936e6cb854d614cdd682f3a344b182ff86
kazuo.kumada <kksiproer@gmail.com>
2018-09-23 16:58:08 +0900

    解答例アクティビティーをコミット

--- app/src/main/java/com/example/sample/quizappli/CorrectSampleActivity.java
+++ app/src/main/java/com/example/sample/quizappli/CorrectSampleActivity.java
@@ -5,38 +5,39 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class CorrectSampleActivity extends AppCompatActivity {
+
+    String tableName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_correct_sample);
-    }
 
-    // Intent取得
-    Intent intent = getIntent();
-    String tableName= intent.getStringExtra("Question");
-    //解説順のid配列を取得
-    int[] id_order = intent.getIntArrayExtra("LIST");
 
-    //DB取得
-    DatabaseHelper helper = new DatabaseHelper(CorrectSampleActivity.this);
-    SQLiteDatabase db = helper.getReadableDatabase();
+        // Intent取得
+        Intent intent = getIntent();
+        tableName = intent.getStringExtra("Question");
+        //解説順のid配列を取得
+        int[] idOrder = intent.getIntArrayExtra("LIST");
 
-    //解説の配列を初期化
-    String[] textlist = new String[10];
+        //DB取得
+        DatabaseHelper helper = new DatabaseHelper(CorrectSampleActivity.this);
+        SQLiteDatabase db = helper.getReadableDatabase();
 
-    //count初期化
-    int count = 0;
 
-    //解説文の初期化
-    String text=null;
+        //count初期化
+        int count = 0;
 
-    //解説を取得
-    try{
-        for(int id : id_order) {
+        //解説文の初期化
+        String text = null;
+
+        StringBuilder sb = new StringBuilder();
+        //解説を取得
+        for (int id : idOrder) {
             String sql = "SELECT TEXT FROM " + tableName + " WHERE _id = " + id;
 
             //SQL実行
@@ -45,16 +46,37 @@ public class CorrectSampleActivity extends AppCompatActivity {
             //データ取得
             while (cursor.moveToNext()) {
                 int indexColumn = cursor.getColumnIndex("TEXT");
-                text = cursor.getString(indexColumn);
+                sb.append("問題");
+                sb.append(count+1);
+                sb.append(": ");
+                sb.append(cursor.getString(indexColumn));
+                text = new String(sb);
             }
 
-            textlist[count]="問題"+count+" :\r\n" + text ;
-
-            TextView textView = findViewById(R.id.text+count);
+            TextView textView = findViewById(R.id.text + count);
             textView.setText(text);
 
             count++;
         }
+
+    }
+
+    public void onClick(View view){
+        int id = view.getId();
+        Intent intent= null;
+        //押下されたボタンで画面遷移を分岐
+        switch (id){
+            case R.id.button_result:
+                intent = new Intent(CorrectSampleActivity.this,ResultActivity.class);
+                intent.putExtra("Question", tableName);
+                break;
+
+            case R.id.button_top:
+                intent = new Intent(CorrectSampleActivity.this,MainActivity.class);
+                break;
+        }
+
+        startActivity(intent);
     }
 
 
