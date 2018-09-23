fcd04c2bf17d4b4b9cdb0cb80da54747bd39f45f
kazuo.kumada <kksiproer@gmail.com>
2018-09-23 11:59:45 +0900

    mainActivityをコミット

--- app/src/main/java/com/example/sample/quizappli/MainActivity.java
+++ app/src/main/java/com/example/sample/quizappli/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sample.quizappli;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 /**
  * 問題選択画面
@@ -51,5 +53,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void feQuestionClick(View view){
+        // テーブル名を持たせる
+        String tableName = "TABLE_FE";
+        //intent作成
+        Intent intent = new Intent(MainActivity.this,QuestionActivity.class);
+        intent.putExtra("Question",tableName);
+        //遷移
+        startActivity(intent);
+    }
+
+    public void javaQuestionClick(View view){
+        // テーブル名を持たせる
+        String tableName = "TABLE_JAVA";
+        //intent作成
+        Intent intent = new Intent(MainActivity.this,QuestionActivity.class);
+        intent.putExtra("Question",tableName);
+        //遷移
+        startActivity(intent);
     }
 }
