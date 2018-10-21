448ac49b9eb0e23dab2b01e0b8b9ce7f39950b37
yuuki shinpo <yuuki.shinpo@gmail.com>
2018-10-21 10:43:38 +0900

    新保： CorrectSampleActivity→TextViewを1つに変更し、ボタンを押下することにいろ次の解説を表示させる仕様に変更

--- app/src/main/java/com/example/sample/quizappli/CorrectSampleActivity.java
+++ app/src/main/java/com/example/sample/quizappli/CorrectSampleActivity.java
@@ -10,8 +10,14 @@ import android.widget.TextView;
 
 public class CorrectSampleActivity extends AppCompatActivity {
 
-    String tableName ;
+    String tableName;
     int[] idOrder;
+    //count初期化
+    int count = 0;
+    int idnumber = 1;
+
+    //解説文の初期化
+    String[] text = new String[11];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +29,16 @@ public class CorrectSampleActivity extends AppCompatActivity {
         Intent intent = getIntent();
         tableName = intent.getStringExtra("Question");
         //解説順のid配列を取得
-        int[] idOrder = intent.getIntArrayExtra("answerOrder");
+        idOrder = intent.getIntArrayExtra("answerOrder");
 
         //DB取得
         DatabaseHelper helper = new DatabaseHelper(CorrectSampleActivity.this);
         SQLiteDatabase db = helper.getReadableDatabase();
 
 
-        //count初期化
-        int count = 0;
 
-        //解説文の初期化
-        String text = null;
-
-        StringBuilder sb = new StringBuilder();
         //解説を取得
-        for (int id = 1 ; id < 11; id++) {
+        for (int id = 1; id < 11; id++) {
 
             String sql = "SELECT KAISETU FROM " + tableName + " WHERE _id = " + idOrder[id];
 
@@ -48,40 +48,52 @@ public class CorrectSampleActivity extends AppCompatActivity {
             //データ取得
             while (cursor.moveToNext()) {
                 int indexColumn = cursor.getColumnIndex("KAISETU");
+                StringBuilder sb = new StringBuilder();
                 sb.append("問題");
                 count++;
                 sb.append(count);
                 sb.append(": ");
                 sb.append(cursor.getString(indexColumn));
-                text = new String(sb);
+                //解説文を解答順に配列に格納
+                text[count] = new String(sb);
             }
-
-
-            TextView textView = findViewById(R.id.text + count);
-            textView.setText(text);
-
         }
-
+        onClicknextkaisetu();
     }
 
-    public void onClick(View view){
+    public void onClick(View view) {
         int id = view.getId();
-        Intent intent= null;
+        Intent intent = null;
         //押下されたボタンで画面遷移を分岐
-        switch (id){
+        switch (id) {
+            case R.id.button_nextkaisetu://最後の問題まで出し切ったらボタンを使用不可能にする
+                onClicknextkaisetu();
+                break;
+
             case R.id.button_result:
-                intent = new Intent(CorrectSampleActivity.this,ResultActivity.class);
+                intent = new Intent(CorrectSampleActivity.this, ResultActivity.class);
                 intent.putExtra("Question", tableName);
-                intent.putExtra("answerOrder",idOrder);
+                intent.putExtra("answerOrder", idOrder);
                 startActivity(intent);
                 break;
 
             case R.id.button_top:
+
                 finish();
+                //AFLG,DFLGの初期化処理を行う
                 break;
         }
     }
 
-
-
+    /*
+     * 次の解説文を表示
+     */
+    public void onClicknextkaisetu() {
+        TextView textView = findViewById(R.id.text1);
+        //textView.setText("　呼ばれました　"+idnumber);
+        textView.setText(text[idnumber]);
+        idnumber++;
+    }
 }
+
+
